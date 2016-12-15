eb41dd6eea99063011e08c7b344f2190d115acfc
Szymon <sd@phase-ii.com>
2016-12-15 17:48:12 +0000

    update

--- server.jsx
+++ server.jsx
@@ -1,9 +1,18 @@
 // https://nodejs.org/dist/latest-v6.x/docs/api/synopsis.html
 
+const Nightmare = require('nightmare');
 const http      = require('http');
 const path      = require('path');
 const log       = console.log;
 const assert    = console.assert;
+const express   = require('express');
+const app = express();
+
+
+Nightmare()
+    .goto('http://yahoo.com')
+    .type('input[title="Search"]', 'github nightmare')
+    .click('.searchsubmit');
 
 assert(process.argv.length > 3, "try to call for example 'node " + path.basename(__filename) + " 0.0.0.0 80'");
 
@@ -19,12 +28,36 @@ const port = process.argv[3];
 
 assert(port >= 0 && port <= 65535, "port beyond range 0 - 65535 : '" + port + "'");
 
-const server = http.createServer((req, res) => {
+app.get('/fetch', (req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+
+    var data = {};
+
+    console.log()
+
+     // Nightmare()
+     //    .goto('http://yahoo.com')
+     //    .type('input[title="Search"]', 'github nightmare')
+     //    .click('.searchsubmit');
+
+    // const nightmare = Nightmare({
+    //     show: true,
+    //     ignoreCertificateErrors: true
+    //     //waitTimeout: (60 * 60 * 24 * 7) * 1000 // one week
+    // });
+    //
+    //
+    // data.url = req.query.url
+    //
+    // nightmare
+    //     .goto(req.query.url)
+    //     // .viewport(win.width, win.height)
+    //     .wait('body')
+    // ;
+    //
+    console.log( ((new Date()).getTime()) + ': ' + JSON.stringify(data))
+    // res.end(JSON.stringify(data));
 });
 
-server.listen(port, ip, () => {
-    log(`Server running at http://${ip}:${port}/`);
-});
+app.listen(port, ip);
