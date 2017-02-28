010c6a87c927b41edc88134653cb0c2b664ed151
SD <szymon.dzialowski@gmail.com>
2017-02-28 01:05:05 +0100

    pre request (head) to check status code and mime type

---
+++ test/head.jsx
@@ -0,0 +1,45 @@
+'use strict';
+
+const url       = require('url');
+const http      = require('http');
+const path      = require('path');
+const log       = require(path.resolve(__dirname, '..', 'lib', 'log.jsx'));
+
+function curl(uri, method) {
+
+    uri = url.parse(uri);
+
+    var options = {
+        method: method,
+        host: uri.hostname,
+        port: uri.port,
+        path: uri.path
+    };
+
+    return new Promise(function (resolve, reject) {
+
+        var req = http.request(options, function(res) {
+
+            res.setEncoding('utf8');
+
+            resolve(res);
+        });
+
+        req.on('error', function(e) {
+            reject(e)
+        });
+
+        req.end();
+    });
+}
+
+curl('http://www.lymphomahub.com/', 'head')
+    .then(function (res) {
+        log.line('status:', res.statusCode)
+        log.json(res.headers)
+    }, function (e) {
+        log.json(e)
+    })
+    .catch(function (e) {
+        log.json(e)
+    })
