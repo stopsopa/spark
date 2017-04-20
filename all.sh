c2480c4463105d4c8160ced50b3d3178e8f94185
simon <grytysek@gmail.com>
2017-04-20 22:20:17 +0200

    all.sh

--- all.sh
+++ all.sh
@@ -9,14 +9,14 @@ sudo killall node &> /dev/null
 sudo killall node &> /dev/null
 sudo killall node &> /dev/null
 
-kill $(ps aux | grep bash | grep -v "$$" | grep -v "  $1" | grep -v "  $2" | awk '{print $2}') &> /dev/null
+kill $(ps aux | grep bash | grep -v grep | grep -v "$$" | grep -v "  $1" | awk '{print $2}') &> /dev/null
 
 if [ "$#" == 0 ] || [ "$#" -gt 1 ] ; then
 	echo "> call: /bin/bash $0 $\$";
 else
     echo "> terminal pid: $1"
     echo "> script   pid: $$"
-    ps aux | grep bash
+    ps aux | grep bash | grep -v grep
     echo '<pre>' > static/log.html
     while true; do killall Xvfb; killall Xvfb; Xvfb -ac -screen scrn 1280x2000x24 :9.0 & disown; echo "<pre>$(date +%Y-%m-%d_%H-%M-%S) Restart Xvfb..." >> static/log.html; sleep $((60 * 10)); done &>> /dev/null & disown
     npm run supervisor 0.0.0.0 8080 &>> /dev/null & disown
@@ -24,8 +24,6 @@ else
 
     echo "> running: node crawler.js lh"
     node crawler.js lh &>> static/log.html & disown
-
-    sleep 4
 
     echo '> running: node crawler.js agp'
     sleep $((60 * 60 * 3)) && node crawler.js agp &>> static/log.html & disown
