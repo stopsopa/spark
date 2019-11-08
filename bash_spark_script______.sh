f44ec0a687dabe307f4efe371f7a71cd27f3673d
stopsopa <grytysek@gmail.com>
2019-11-08 19:49:12 +0000

    up

--- bash_spark_script______.sh
+++ bash_spark_script______.sh
@@ -5,6 +5,6 @@ cat << EOF
 Now run two commands to restart prerender:
 
     /bin/bash all.sh
-    while true; do /bin/bash cron.sh $$; sleep 120; done & disown
+    while true; do /bin/bash cron.sh $$; sleep 86400; done & disown
 
 EOF
